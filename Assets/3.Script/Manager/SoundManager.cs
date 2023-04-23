4d2ebaba336551ff2f033785691f7d96bad2d3b2
hyeonjunje <88482159+hyeonjunje@users.noreply.github.com>
2023-04-23 23:59:33 +0900

    Add bgm
    bgm 넣었습니다.(코드만)

--- Assets/3.Script/Manager/SoundManager.cs
+++ Assets/3.Script/Manager/SoundManager.cs
@@ -6,8 +6,12 @@ using UnityEngine;
 public enum EBGM
 {
     Menu,
-    Level1
-        
+    Level1,
+    Merchant,
+    EliteMeet,
+    PlayerDead,
+    BossMeet,
+    BossClear,
 }
 
 public class SoundManager
@@ -29,6 +33,11 @@ public class SoundManager
 
         _dictionary[EBGM.Menu] = Resources.Load<AudioClip>("music/STS_MenuTheme_NewMix_v1");
         _dictionary[EBGM.Level1] = Resources.Load<AudioClip>("music/STS_Level1_NewMix_v1");
+        _dictionary[EBGM.Merchant] = Resources.Load<AudioClip>("music/STS_Merchant_NewMix_v1");
+        _dictionary[EBGM.EliteMeet] = Resources.Load<AudioClip>("music/STS_EliteBoss_NewMix_v1");
+        _dictionary[EBGM.PlayerDead] = Resources.Load<AudioClip>("music/STS_DeathStinger_1_v3_MUSIC");
+        _dictionary[EBGM.BossMeet] = Resources.Load<AudioClip>("music/STS_Boss1_NewMix_v1");
+        _dictionary[EBGM.BossClear] = Resources.Load<AudioClip>("music/STS_BossVictoryStinger_1_v3_MUSIC");
     }
 
     public void PlayBGM(EBGM bgm)
